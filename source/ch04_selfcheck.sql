dec4291fc22a21ae7c9d0d68c051644842f866a4
HyunJi Yoo <dbguswl941@gmail.com>
2025-09-25 11:15:52 +0900

    UP

--- source/ch04_selfcheck.sql
+++ source/ch04_selfcheck.sql
@@ -22,14 +22,18 @@
 -- salary: 직원의 연봉으로, 단위는 '천만 원'이며, 정수로 작성합니다.
 
 -- 1. company DB를 생성 및 진입한 후 employees 테이블을 만드세요.
+CREATE DATABASE company;
+USE company;
+CREATE TABLE employees(
+ id INT PRIMARY KEY,
+ name VARCHAR(50), 
+ department VARCHAR(50),
+ salary INT 
+);
 -- 2. employees 테이블에 직원 10명의 데이터를 삽입하세요.
--- 3. 모든 직원의 연봉 합계를 계산하세요.
--- 4. Sales 부서의 평균 연봉을 구하세요.
--- 5. 부서가 모두 몇 개 있는지 조회하세요.
--- 6. Sales 부서의 최대 연봉과 최소 연봉 차이를 구하세요.
--- 7. 가장 높은 연봉을 받는 직원은 전체 평균 연봉 대비 얼마를 더 받는지 구하세요.
-
-	(101, 'John', 'Sales', 7000),
+INSERT INTO employees (id, name, department, salary)
+VALUES
+(101, 'John', 'Sales', 7000),
 	(102, 'Aria', 'IT', 5500),
 	(103, 'Mike', 'Sales', 8000),
 	(104, 'Lily', 'HR', 6500),
@@ -39,5 +43,28 @@
 	(108, 'Sophia', 'HR', 6300),
 	(109, 'Lucas', 'Sales', 5500),
 	(110, 'Charlotte', 'HR', 6800);
+    
+SELECT* FROM employees;
+-- 3. 모든 직원의 연봉 합계를 계산하세요.
+SELECT SUM(salary)
+FROM employees;
+-- 4. Sales 부서의 평균 연봉을 구하세요.
+SELECT AVG(salary)
+FROM employees
+WHERE department = 'Sales';
+-- 5. 부서가 모두 몇 개 있는지 조회하세요.
+SELECT COUNT(department)
+FROM employees;
+-- 6. Sales 부서의 최대 연봉과 최소 연봉 차이를 구하세요.
+SELECT MAX(salary), MIN(salary)
+FROM employees
+WHERE department = 'Sales';
+-- 7. 가장 높은 연봉을 받는 직원은 전체 평균 연봉 대비 얼마를 더 받는지 구하세요.
+SELECT MAX(salary) - AVG(salary)
+FROM employees; 
+
+
+
+
 
 
