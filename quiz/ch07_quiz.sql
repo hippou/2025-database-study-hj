0ff29855d68d3dd8cc2e30a502caa96eb400dab6
HyunJi Yoo <dbguswl941@gmail.com>
2025-09-29 17:34:20 +0900

    up

--- quiz/ch07_quiz.sql
+++ quiz/ch07_quiz.sql
@@ -20,6 +20,7 @@
 -- ④ UNION ALL 연산자를 사용했을 때와 결과가 같다.
 
 -- 정답: 4
+-- 결과를 합치고 중복 레코드를 제거하는 UNION 연산자를 사용했을 때와 결과가 같음
 
 
 -- 문제 3
@@ -30,7 +31,7 @@
 -- ③ 두 테이블의 결과 집합에 포함된 칼럼 수와 각 칼럼의 자료형이 동일해야 한다.
 -- ④ UNION 연산자는 테이블 간의 관계를 기반으로 데이터를 결합한다.
 
--- 정답: 
+-- 정답: 3
 
 
 -- 문제 4
@@ -41,7 +42,7 @@
 -- ③ 데이터의 중복을 제거하기 위해
 -- ④ 여러 테이블 간의 관련 데이터를 결합해 의미 있는 정보를 추출하기 위해
 
--- 정답: 
+-- 정답: 4
 
 
 -- 문제 5
@@ -59,12 +60,15 @@
 --   FROM 테이블1 
 --   LEFT JOIN 테이블2;
 
--- 정답: 
+-- 정답: 3
 
 
 -- 2. 조인 연습 문제
 
 -- 다음 도서관 데이터베이스를 보고 문제에 답하세요.
+
+CREATE DATABASE library;
+USE library;
 
 -- members 테이블
 CREATE TABLE members (
@@ -158,35 +162,48 @@ VALUES
 -- 모든 회원의 이름, 생년월일, 주소를 조회하세요.(회원 프로필이 없는 회원은 제외)
 
 -- 정답:
-
+SELECT name, date_of_birth, address
+FROM members m
+JOIN member_profiles mp ON m.id = mp.member_id;
 
 
 -- 문제 2
 -- 모든 회원의 이름, 생년월일, 주소를 조회하세요.(회원 프로필이 없는 경우 NULL로 출력)
 
 -- 정답:
-
+SELECT name, date_of_birth, address
+FROM members m
+LEFT JOIN member_profiles mp ON m.id = mp.member_id;
 
 
 -- 문제 3
 -- 대출 기록을 보고 도서를 빌려 간 회원명과 대출한 도서명을 조회하세요.
 
 -- 정답:
-
+SELECT m.name, b.title
+FROM borrow_records br
+JOIN members m ON br.member_id = m.id
+JOIN books b ON br.book_id = b.id;
 
 
 -- 문제 4
 -- 모든 도서명과 대출 날짜를 조회하세요.(대출되지 않은 도서는 NULL로 출력)
 
 -- 정답:
-
+SELECT b.title, br.borrow_date
+FROM books b
+LEFT JOIN borrow_records br ON b.id = br.book_id;
 
 
 -- 문제 5
 -- 회원과 같은 이름을 가진 직원의 이름과 역할을 조회하세요.
 
 -- 정답:
-
+SELECT ls.name, ls.role
+FROM members m
+JOIN library_staff ls ON m.name = ls.name;
+-- 관계 설정 없이도 JOIN을 할 수 있다는 것을 보여줌
+-- 일반적으로는 관계 있는 테이블끼리 JOIN 수행
 
 
 -- 문제 6
@@ -194,5 +211,7 @@ VALUES
 -- 모든 회원과 도서관 직원의 이름을 하나의 목록으로 출력하세요.(중복된 이름은 제거)
 
 -- 정답:
-
+SELECT name FROM members
+UNION
+SELECT name FROM library_staff;
 
